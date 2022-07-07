6a5f6234303570402d20f295a40cb8b12a7c74e4
Hayley Brinicombe <103518155+HayleyBrinicombe@users.noreply.github.com>
2022-07-07 11:03:12 +0100

    Task 9 complete endpoints, tests, model controller

--- server/models/games-models.js
+++ server/models/games-models.js
@@ -1,5 +1,5 @@
+const { connect } = require("../../db/connection");
 const connection = require("../../db/connection");
-const reviews = require("../../db/data/test-data/reviews");
 
 exports.selectCategories = () => {
   return connection.query("SELECT * FROM categories;").then(({ rows }) => {
@@ -67,4 +67,33 @@ exports.selectReviews = () => {
     .catch((err) => {
       return err;
     });
+};
+exports.selectCommentsByReviewId = async (review_id) => {
+  if (isNaN(+review_id)) {
+    return Promise.reject({
+      status: 400,
+      msg: "Invalid input"
+    });
+  }
+
+  const reviewCheck = await connection.query(
+    `SELECT * FROM comments
+     WHERE review_id = $1`,
+    [review_id]
+  );
+  if (reviewCheck.rowCount === 0) {
+    return Promise.reject({
+      status: 404,
+      msg: "Resource not found"
+    });
+  }
+
+  const comments = await connection.query(
+    `SELECT * FROM comments
+     WHERE review_id = $1`,
+    [review_id]
+  );
+  
+
+  return comments.rows;
 };
